ce09a1e574b831bf43552c3aa393309b38754e01
Salarionhell <150520413+Salarionhell@users.noreply.github.com>
2024-04-30 16:21:28 +0300

    Update new1.sh

--- new1.sh
+++ new1.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 read input_dir
 read output_dir
 find $input_dir -type f -name "*.txt" | while read file; do
   filename=$(basename "$file")
   destination="$output_dir/$filename"
   
-  if [ -f "$destination" ]; then
+  if [[ -f "$destination" ]]; then
     counter=1
-    while [ -f "$destination" ]; do
+    while [[ -f "$destination" ]]; do
       new_filename="${filename%.*}_$counter.${filename##*.}"
       destination="$output_dir/$new_filename"
       ((counter++))
