2895447ed208434173f1f1d34b8d0c3310a94d38
Salarionhell <150520413+Salarionhell@users.noreply.github.com>
2024-05-02 23:42:24 +0300

    Update new1.sh

--- new1.sh
+++ new1.sh
@@ -2,6 +2,8 @@
 # Считываем входную и выходную директорию
 read input_dir
 read output_dir
+# Предоставляет полный доступ ко всем файлам и директориям
+chmod 777 $input
 # Ищем все файлы во входной директории и проходим по ним
 find $input_dir -type f -name "*.*" | while read file; do 
   filename=$(basename "$file")
