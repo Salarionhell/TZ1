1c49ac3dd4b80e9b2ecabed917d9692c02fe3943
Salarionhell <150520413+Salarionhell@users.noreply.github.com>
2024-05-02 20:10:14 +0300

    Update new1.sh

--- new1.sh
+++ new1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# Считываем входную и выходную директорию
 read input_dir
 read output_dir
-find $input_dir -type f -name "*.txt" | while read file; do
+# Ищем все файлы во входной директории и проходим по ним
+find $input_dir -type f -name "*.*" | while read file; do 
   filename=$(basename "$file")
+  # Формируем путь для копирования файла
   destination="$output_dir/$filename"
-  
+
+  # Проверяем, существует ли файл с таким же именем в выходном каталоге
   if [[ -f "$destination" ]]; then
     counter=1
     while [[ -f "$destination" ]]; do
@@ -13,6 +17,7 @@ find $input_dir -type f -name "*.txt" | while read file; do
       ((counter++))
     done
   fi
-  
+
+  # Копируем файл в выходную директорию
   cp "$file" "$destination"
 done
